de662ca49d781556a9b894989d0b55344f0ebe56
Kseniya Lomakina <kl@zili.ru>
2021-01-22 11:14:00 +0300

    popup done

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 let openPopup = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.overlay');
+let popup = document.querySelector('.popup');
 let closePopup = document.querySelector('.popup__close-button');
 
 let togglePopup = function() {
 
-  popup.classList.toggle('overlay_display_active');
+  popup.classList.toggle('popup_opened');
 }
 
 openPopup.addEventListener('click', togglePopup);
@@ -16,5 +16,31 @@ popup.addEventListener('click', function(event) {
   if (event.target === event.currentTarget)
   togglePopup();
 })
+
+
+let formElement = document.querySelector('.popup__container');
+
+let nameInput = document.querySelector('.popup__user-name');
+let jobInput = document.querySelector('.popup__user-job');
+
+let nameField = document.querySelector('.profile__title')
+let jobField = document.querySelector('.profile__subtitle')
+
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+
+  nameField.textContent = nameInput.value;
+  jobField.textContent = jobInput.value;
+
+  popup.classList.remove('popup_opened');
+}
+
+
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
 
 
