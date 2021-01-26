a2d7ba385f6eafe4c6854144af175af83bbba6a2
Kseniya Lomakina <kl@zili.ru>
2021-01-26 16:30:51 +0300

    исправить ошибки в js

--- scripts/index.js
+++ scripts/index.js
@@ -9,42 +9,35 @@ let jobInput = document.querySelector('.popup_job_data');
 let nameElement = document.querySelector('.profile__title')
 let jobElement = document.querySelector('.profile__subtitle')
 
+// Получаем текстовое содержимое из профиля
+
 let getProfileData = function () {
   let name = nameElement.textContent;
   let job = jobElement.textContent;
   return { name, job };
 }
 
+// Открываем попап и вставляем данные из профиля в поля Input
 let openPopup = function () {
   let profileData = getProfileData();
   fillPopupForm(profileData);
   popupElement.classList.add('popup_display_opened');
 }
 
+// Объявляем функцию, которая вставляет текстовое содержимое в поля Input
+
 function fillPopupForm(profileData) {
   nameInput.value = profileData.name;
   jobInput.value = profileData.job;
 }
 
+// Закрываем попап
+
 let closePopup = function () {
   popupElement.classList.remove('popup_display_opened');
 }
 
-
-openPopupElement.addEventListener('click', openPopup);
-
-closePopupElement.addEventListener('click', closePopup);
-
-popupElement.addEventListener('mousedown', function (event) {
-  if (event.target === event.currentTarget)
-    closePopup();
-})
-
-
-
-
-formElement.addEventListener('submit', formSubmitHandler);
-
+//  Вставляем введенную информацию в профиль, закрываем попап нажатием на кнопку "сохранить"
 
 function formSubmitHandler(event) {
   event.preventDefault();
@@ -52,10 +45,30 @@ function formSubmitHandler(event) {
   closePopup();
 }
 
+// Объявляем функцию, которая вставляет введенные данные в профиль пользователя
 function setProfileData() {
   nameElement.textContent = nameInput.value;
   jobElement.textContent = jobInput.value;
+
 }
+
+
+openPopupElement.addEventListener('click', openPopup);
+
+closePopupElement.addEventListener('click', closePopup);
+
+//  Закрываем попап нажатием на дисплей
+popupElement.addEventListener('mousedown', function (event) {
+  if (event.target === event.currentTarget)
+    closePopup();
+})
+
+
+// Отправляем форму
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
 
 
 
