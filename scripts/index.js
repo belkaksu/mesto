db747a518833aea66bde671f0b0d8da105408339
Kseniya Lomakina <kl@zili.ru>
2021-02-07 00:59:36 +0300

    refactor: сделать универсальным открытие и закрытие попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const profilePopupOpenButton = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const PopupCloseButton = document.querySelector('.close-button');
+const profilePopupElement = document.querySelector('.popup-profile')
 const profileFormElement = document.querySelector('.popup__container_profile_data');
 
 const profileNameInput = document.querySelector('.popup__item_data_title');
@@ -34,13 +35,13 @@ function fillProfilePopupForm() {
 // Открываем попап и вставляем данные из профиля в поля Input
 const openProfilePopup = function () {
   fillProfilePopupForm();
-  openPopup(popupElement);
+  openPopup(profilePopupElement);
 }
 
 // Закрываем попап
 
 const closeProfilePopup = function () {
-  closePopup(popupElement);
+  closePopup(profilePopupElement);
 }
 
 // Объявляем функцию, которая вставляет введенные данные в профиль пользователя
@@ -104,7 +105,8 @@ const imagePopupTextElement = imagePopup.querySelector('.popup-image__subtitle')
 // открытие popup-image
 
 function openImagePopup(link, name) {
-  imagePopup.classList.add('popup-image_display_opened');
+ 
+  openPopup(imagePopup);
   imagePopupImageElement.src = link;
   imagePopupTextElement.textContent = name;
   imagePopupTextElement.alt = name;
@@ -113,7 +115,7 @@ function openImagePopup(link, name) {
 // закрытие popup-image
 
 function closeImagePopup() {
-  imagePopup.classList.remove('popup-image_display_opened');
+  closePopup(imagePopup);
 }
 
 imagePopupCloseButton.addEventListener('click', closeImagePopup);
@@ -164,7 +166,7 @@ render()
 const popupNewCardOpenButton = document.querySelector('.profile__add-button');
 const popupNewCardElement = document.querySelector('.popup-add-card');
 const popupNewCardCloseButton = popupNewCardElement.querySelector('.close-button');
-const popupNewCardFormElement = popupNewCardElement.querySelector('.popup__container_add_card');
+const popupNewCardFormElement = popupNewCardElement.querySelector('.popup-add-card__container');
 
 const newCardNameInput = popupNewCardElement.querySelector('.popup__item_data_name');
 const newCardPlaceInput = popupNewCardElement.querySelector('.popup__item_data_place');
