5f118b5d21adf49c9520e7d17f74912665265990
Kseniya Lomakina <kl@zili.ru>
2021-04-02 17:12:30 +0300

    переименовать файл Card.js

--- scripts/card.js
+++ scripts/card.js
@@ -38,7 +38,6 @@ export class Card {
     this._element.querySelector('.element__image').addEventListener('click', () => {
       this._handleCardClick(this._link, this._name);
     });
-
   }
 
   // Ставим лайк
