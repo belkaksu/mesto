69ba878fc734d0d5e1a466e65b59914467639b58
Kseniya Lomakina <kl@zili.ru>
2021-07-13 00:57:38 +0300

    class Section

--- scripts/components/Section.js
+++ scripts/components/Section.js
@@ -1,4 +1,17 @@
-export default class Section = {
+export default class Section {
 
+constructor({ item, renderer }, containerSelector) {
+  this._renderedItems = item;
+  this._renderer = renderer;
+  this._container = document.querySelector(containerSelector);
 }
+renderItems() {
+  this._renderedItems.forEach(item => this._renderer(item));
+}
+addItem() {
+  this._container.append(element);
+}
+}
+
+
 
