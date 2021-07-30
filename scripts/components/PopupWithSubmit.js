98e657c05010b761bb289ae7d16281d1c9e27ff4
Kseniya Lomakina <kl@zili.ru>
2021-07-30 23:36:13 +0300

    class PopupWithSubmit

--- scripts/components/PopupWithSubmit.js
+++ scripts/components/PopupWithSubmit.js
@@ -1,7 +1,22 @@
 import Popup from "./Popup";
 
 export default class PopupWithSubmit extends Popup {
-  constructor(popupSelector, submitCallback)
-}
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._form = popupSelector.querySelector('.popup__container');
+    this._handleSubmitCallback = null;
+  }
 
+  setFormSubmit(handler) {
+    this._handleSubmitCallback = handler;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      this._handleSubmitCallback()
+    });
+  }
+}
 
